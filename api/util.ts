aca366fe882e284120b2facafb5e28163b0894bf
blesstosam <805902285@qq.com>
2020-03-26 10:32:43 +0800

    fix axios util

--- api/util.ts
+++ api/util.ts
@@ -1,15 +1,19 @@
 import axios, { AxiosResponse, AxiosRequestConfig, AxiosError, AxiosInstance } from 'axios';
 // import store from '../store';
-// import { LOGOUT } from '@/store/mutation-types';
 import Vue from 'vue';
-// import storage from '@/assets/script/storage';
+// import { LOGOUT } from '~/store/mutation-types';
+// import cfg from '@/config/index';
 
 let dispathLogoutTime = 0; // 确保logout只调用了一次
 
 export class HttpService {
   service: AxiosInstance;
 
-  constructor(url: string) {
+  constructor(
+    url: string,
+    requestCb?: (config: AxiosRequestConfig) => void,
+    responseCb?: (response: AxiosResponse) => void
+  ) {
     this.service = axios.create({
       baseURL: url,
       timeout: 30000,
@@ -18,9 +22,15 @@ export class HttpService {
     // request拦截器
     this.service.interceptors.request.use(
       (config: AxiosRequestConfig) => {
+        // let token = (store.state as any).user.token;
         // if (token) {
         //   config.headers.token = token;
         // }
+        // const _locale = (store.state as any).app.local;
+        // config.headers.locale = _locale === cfg.langType.CN ? 'zh_CN' : 'en_US';
+
+        // 将单独的逻辑通过回调的方式给外部处理
+        requestCb && requestCb(config);
 
         return config;
       },
@@ -32,26 +42,39 @@ export class HttpService {
     // respone拦截器
     this.service.interceptors.response.use(
       (response: AxiosResponse) => {
-        if (response.status !== 200 && response.status !== 201) {
+        if (response.status !== 200) {
           // 405: 其他客户端登录了;  407:Token 过期了;
           if (response.status !== 407 && response.status !== 405) {
             Vue.prototype.$Message.error('网络异常，请稍后重试');
           } else if (dispathLogoutTime === 0) {
             dispathLogoutTime++;
-            // storage.set('logoutMsg', response.data.msg);
-            // store.dispatch(LOGOUT);
+            // Vue.prototype.$Modal.info({
+            //   title: response.data.msg,
+            //   onOk: () => {
+            //     store.dispatch(LOGOUT);
+            //   }
+            // });
           }
           return Promise.reject(response);
         }
+
         // 405: 其他客户端登录了;  407:Token 过期了;
         if (response.data.code === 407 || response.data.code === 405) {
           if (dispathLogoutTime === 0) {
             dispathLogoutTime++;
-            // storage.set('logoutMsg', response.data.msg);
-            // store.dispatch(LOGOUT);
+            // Vue.prototype.$Modal.info({
+            //   title: response.data.msg,
+            //   onOk: () => {
+            //     store.dispatch(LOGOUT);
+            //   }
+            // });
           }
+
           return Promise.reject(response); // 407的直接不处理
         }
+
+        responseCb && responseCb(response);
+
         return response; // return 会默认resolve
       },
       (error: AxiosError) => {
@@ -61,7 +84,7 @@ export class HttpService {
           // 处理timeout
           return Promise.reject(error);
         }
-        /* eslint-disable-next-line */
+        /* eslint-disable-next-line  */
         return Promise.reject({ msg: '网络异常，请稍后重试' });
       }
     );
@@ -104,18 +127,17 @@ export class HttpService {
    * @param {formData对象} data
    */
   upload(url: string, data: any): Promise<any> {
-    /* eslint-disable-next-line */
-    return new Promise((reslove) => {
+    return new Promise((resolve) => {
       this.service
         .post(url, data, {
           headers: { 'Content-Type': 'multipart/form-data;boundary = ' + new Date().getTime() },
         })
         .then(
-          (res: any) => {
-            reslove(res.data);
+          (res) => {
+            resolve(res.data);
           },
-          (err: any) => {
-            reslove(err);
+          (err) => {
+            resolve(err);
           }
         );
     });
