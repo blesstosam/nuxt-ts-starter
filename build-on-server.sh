f320e6031579eb78bab13ef9488fdadb4348d1dd
blesstosam <805902285@qq.com>
2020-03-27 17:37:29 +0800

    add restart script

--- build-on-server.sh
+++ build-on-server.sh
@@ -6,6 +6,19 @@ deploy_branch="master"
 # 部署的位置
 deploy_path="/home/admin/node" 
 
+function restartPm2Script() {
+  NAME=$1;
+  SCRIPT=${2:-start}
+  pm2 describe $NAME > /dev/null
+  RUNNING=$?
+  if [ "${RUNNING}" -ne 0 ]; then
+    echo "start app '$NAME'..."
+    pm2 start npm --name $NAME -- run $SCRIPT
+  else
+    echo "restart app '$NAME'..."
+    pm2 restart $NAME
+  fi;
+}
 
 if [ ! -d "${deploy_path}/${repo_name}" ];then
 echo "--------------------------- git 初始化仓库 ------------------------"
@@ -20,10 +33,10 @@ fi
 # 使用ts-node直接启动程序
 command -v pm2 >/dev/null 2>&1 || { echo >&2 "未安装pm2"; exit 1; }
 echo "--------------------------- npm run publish:ts ------------------------"
-pm2 start npm --name ${repo_name} -- run publish:ts
+restartPm2Script ${repo_name} publish:ts
 
 # 先打包再启动
 # todo 这个不会打包koa的代码
-command -v pm2 >/dev/null 2>&1 || { echo >&2 "未安装pm2"; exit 1; }
-echo "--------------------------- npm run publish ---------------------------"
-pm2 start npm --name ${repo_name} -- run publish
+# command -v pm2 >/dev/null 2>&1 || { echo >&2 "未安装pm2"; exit 1; }
+# echo "--------------------------- npm run publish ---------------------------"
+# restartPm2Script ${repo_name} publish
